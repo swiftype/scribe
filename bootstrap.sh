a18411a55b2d29f097adba41436bbb008aad6a4e
Hans Duedal <hd@onlinecity.dk>
2011-11-11 01:15:59 +0100

    Added check for thrift version 0.7+

--- bootstrap.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-thrift -o src --gen cpp:pure_enums if/fb303.thrift
-thrift -o src --gen cpp:pure_enums if/bucketupdater.thrift
-thrift -o src --gen cpp:pure_enums if/scribe.thrift
-cmake .
